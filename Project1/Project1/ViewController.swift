18203acc87673e030332d6b6ad4aa5fa0ea32bd7
Sanskar Jethi <sansyrox@gmail.com>
2019-06-06 03:57:03 +0530

    Project one

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Sanskar Jethi on 06/06/19.
+//  Copyright © 2019 Sanskar Jethi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+        }
+
+}
+
