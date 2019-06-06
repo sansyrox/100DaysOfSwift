f21c8f4c0378d08e976d23d6b0669a0a4c803aa8
Sanskar Jethi <sansyrox@gmail.com>
2019-06-07 03:03:13 +0530

    Project 1 COmpleted

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         // Do any additional setup after loading the view, typically from a nib.
         
         let fm = FileManager.default
@@ -27,17 +31,27 @@ class ViewController: UITableViewController {
             }
         }
         
-        print(pictures)
+        pictures.sort()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = "Picture \(indexPath.row+1) of \(pictures.count)"
+        cell.textLabel?.font = UIFont(name: "SFMono", size: 17)
         return cell
         }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
 
