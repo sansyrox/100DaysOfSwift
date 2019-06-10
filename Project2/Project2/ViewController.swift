18363aac2153542b2ac90efb82bcd37955ed605c
Sanskar Jethi <sansyrox@gmail.com>
2019-06-11 00:37:43 +0530

    Project 2 finished

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
+    var questions=1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,10 +37,51 @@ class ViewController: UIViewController {
     }
 
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = "\(countries[correctAnswer].uppercased()) .Score is \(score)"
+        
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        questions+=1
+        
+        if(questions==10){
+            let bc = UIAlertController(title: "10 Questions have been asked", message: "", preferredStyle: .alert)
+            bc.addAction(UIAlertAction(title: "Start Again?", style: .default, handler: { (UIAlertAction) in
+                self.score = 0;
+                self.askQuestion()
+            }))
+            
+            present(bc, animated: true)
+            return
+        }
+        
+        
+        var title:String
+        
+        if(sender.tag==correctAnswer){
+            score+=1
+            title="Correct"
+        }else {
+            title="Wrong. Correct answer is \(countries[correctAnswer].uppercased())"
+            score-=1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+        
+        
+    }
+    
 }
 
