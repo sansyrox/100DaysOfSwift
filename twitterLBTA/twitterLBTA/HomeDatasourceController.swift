4436b7e5c3e879bee90d40e4c22d7a81f3858ebb
Sanskar Jethi <sansyrox@gmail.com>
2019-07-16 03:49:49 +0530

    Try 300876 at building apps w/o storyboard

---
+++ twitterLBTA/twitterLBTA/HomeDatasourceController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeDatasourceController.swift
+//  twitterLBTA
+//
+//  Created by Sanskar Jethi on 16/07/19.
+//  Copyright © 2019 Sanskar Jethi. All rights reserved.
+//
+
+//import LBTAComponents
+//
+//class HomeDataSource: Datasource {
+//    let words = ["user1","user2","user3"]
+//    
+//    override func numberOfItems(_ section: Int) -> Int {
+//        return words.count
+//    }
+//}
+//
+//class HomeDataSourceController: DatasourceController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        collectionView.backgroundColor = .red
+//        let homeDataSource = HomeDataSource()
+//        self.datasource = homeDataSource
+//    }
+//}
---
+++ twitterLBTA/twitterLBTA/homeController.swift
@@ -0,0 +1,103 @@
+//
+//  homeController.swift
+//  twitterLBTA
+//
+//  Created by Sanskar Jethi on 16/07/19.
+//  Copyright © 2019 Sanskar Jethi. All rights reserved.
+//
+
+import UIKit
+
+class WordCell: UICollectionViewCell {
+    
+    // this gets called when cell is dequed
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        
+    }
+    
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text="TEXT TEXT TEXT"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        backgroundColor = .yellow
+        addSubview(wordLabel)
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellid = "cellid"
+    let headerId="headerId"
+    let footerId = "footerId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        collectionView.register(WordCell.self, forCellWithReuseIdentifier: cellid)
+        collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: footerId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellid, for: indexPath)
+//        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    
+    // to add headert and footer
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader{
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: self.headerId, for: indexPath)
+            header.backgroundColor = .blue
+            return header
+        }
+        else {
+            let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerId, for: indexPath)
+            footer.backgroundColor = .green
+            return footer
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    
+//    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+//        let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+//        header.backgroundColor = .blue
+//        return header
+//    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//        return CGSize(width: view.frame.width, height: 50)
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+}
